e81ce5813a291e07eee998b2af8ce54591dcec75
곽채연 <cheyeon001214@naver.com>
2023-03-27 21:40:44 +0900

    26강의 중간저장

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,12 @@
 "use strict";
 
-class UserStorage {
-    static #users = {
-        id: ["codus", "woorimIT", "김팀장"],
-        psword: ["1234", "1234", "123456"],
-        name: ["챈","우리밋","김팀장"],
+const fs= require("fs");
 
-    };
+class UserStorage {
+    
 
     static getUsers(...fields){
-        const users = this.#users;
+        //const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => { 
             //newUsers에는 fields의 초기값이 들어가게 되는데, 
             //이 초기값은 내가 지정할 수 있고 다음 변수들은 field에 들어오게 된다
@@ -23,7 +20,7 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
+        //const users = this.#users;
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUser,info) => {
@@ -35,7 +32,7 @@ class UserStorage {
     }
 
     static save(userInfo) {
-        const users = this.#users;
+       // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
