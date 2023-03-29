5be30319671716fdcc1d99e99fa70554208fe6cb
곽채연 <cheyeon001214@naver.com>
2023-03-29 17:37:27 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,9 +1,20 @@
 "use strict";
 
-const fs= require("fs");
+const fs= require("fs").promises;
 
 class UserStorage {
-    
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data)
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser,info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+    }, {});
+   
+    return userInfo;
+    }
+
 
     static getUsers(...fields){
         //const users = this.#users;
@@ -20,15 +31,13 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        //const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser,info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+      return fs
+        .readFile("./src/databases/users.json")
+        .then((data)=> {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+        
     }
 
     static save(userInfo) {
